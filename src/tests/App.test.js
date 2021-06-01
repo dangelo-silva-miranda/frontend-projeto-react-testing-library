460ea7edcba3ac1adcf5a9e8b8274d9406491ff8
Dângelo-Silva-Miranda <dangelo.silva.miranda@gmail.com>
2021-06-01 19:03:10 -0300

    add: add helper renderWithRouter function . Modified file: renderWithRouter.jsx

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,15 +1,24 @@
 import React from 'react';
 import { MemoryRouter } from 'react-router-dom';
 import { render } from '@testing-library/react';
+
 import App from '../App';
+import renderWithRouter from './renderWithRouter';
 
 describe('Test the App component', () => {
-  test('renders a reading with the text `Pokédex`', () => {
-    const { getByText } = render(
-      <MemoryRouter>
-        <App />
-      </MemoryRouter>,
+  test('renders a heading with the text `Pokédex`', () => {
+    const { getByText } = renderWithRouter(
+      // <MemoryRouter>
+      <App />,
+      // </MemoryRouter>,
     );
+
+    /*
+      Forma equivalente usando <MemoryRouter>:
+
+      const { getByText } = renderWithRouter(<App />);
+      history.push('/');
+    */
     const heading = getByText(/Pokédex/i);
     expect(heading).toBeInTheDocument();
   });
@@ -20,7 +29,28 @@ describe('Test the App component', () => {
         <App />
       </MemoryRouter>,
     );
+    /*
+      Forma equivalente usando renderWithRouter:
+
+      const { getByText, history } = renderWithRouter(<App />);
+      history.push('/');
+    */
 
     expect(getByText('Encountered pokémons')).toBeInTheDocument();
   });
+
+  describe(
+    'checks if the top of the application contains a fixed set of navigation links',
+    () => {
+      test.only('if the first link has the text Home', () => {
+        const { getByRole } = render(
+          <MemoryRouter>
+            <App />
+          </MemoryRouter>,
+        );
+        const homeLink = getByRole('link', { name: /^home$/i });
+        expect(homeLink).toBeInTheDocument();
+      });
+    },
+  );
 });
