e458b353ac785e7358004b95da2395736ac26c3e
Dângelo-Silva-Miranda <dangelo.silva.miranda@gmail.com>
2021-06-02 01:24:48 -0300

    add: test if the page contains two paragraphs with text about a Pokédex. Modified file: About.test.js

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -13,4 +13,19 @@ describe(`Test the About component: test if the page
       const heading = getByRole('heading', { level: 2, name: /^About Pokédex$/i });
       expect(heading).toBeInTheDocument();
     });
+
+  /*
+      Créditos pela sugestão do getByText ao pesquisar em paragráfos: Paulo R Zambelli
+
+      Fonte: https://testing-library.com/docs/react-testing-library/api/#container-1
+    */
+  test.only('if the page contains two paragraphs with text about a Pokédex',
+    () => {
+      const { container } = render(
+        <About />,
+      );
+      const paragraph = container.querySelectorAll('p');
+
+      expect(paragraph.length).toBe(2);
+    });
 });
