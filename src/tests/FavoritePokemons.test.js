ff8940e4669fa322867ff9bff87a6794a12bdd3c
Dângelo-Silva-Miranda <dangelo.silva.miranda@gmail.com>
2021-06-02 03:49:22 -0300

    add: test if the message No favorite pokemon found is displayed on the screen, if the person does not have favorite pokemons. Modified file: FavoritePokemons.test.js

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,17 @@
-test('', () => {});
+import React from 'react';
+import { render } from '@testing-library/react';
+import { FavoritePokemons } from '../components';
+
+describe('Test the FavoritePokemons component', () => {
+  test.only(`if the message No favorite pokemon found is displayed on 
+  the screen, if the person does not have favorite pokemons`,
+  () => {
+    const { container } = render(
+      <FavoritePokemons />,
+    );
+
+    const paragraph = container.querySelector('p');
+
+    expect(paragraph).toHaveTextContent(/^No favorite pokemon found$/i);
+  });
+});
