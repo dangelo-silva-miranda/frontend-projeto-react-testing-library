de1b525517474c1ef7dc743829da9af2bd49d6ab
Dângelo-Silva-Miranda <dangelo.silva.miranda@gmail.com>
2021-06-01 23:51:30 -0300

    add: test if the page contains a heading h2 with the text About Pokédex. Modified file: About.test.js

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -4,7 +4,7 @@ import { About } from '../components';
 
 describe(`Test the About component: test if the page
  contains information about Pokédex`, () => {
-  test('',
+  test('if the page contains a heading h2 with the text About Pokédex',
     () => {
       const { getByRole } = render(
         <About />,
