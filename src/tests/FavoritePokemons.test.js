f9a84e7492a3d5a25674d403657a4e4e31653015
Dângelo-Silva-Miranda <dangelo.silva.miranda@gmail.com>
2021-06-02 12:20:51 -0300

    add: test if all favorite Pokemon cards are displayed. Modified file: FavoritePokemons.test.js

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,9 +1,18 @@
 import React from 'react';
-import { render } from '@testing-library/react';
-import { FavoritePokemons } from '../components';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 
+import { FavoritePokemons } from '../components';
+import App from '../App';
+import renderWithRouter from './renderWithRouter';
+
+/*
+  Material consultado sobre verificar se checkbox está checked
+  https://stackoverflow.com/a/55178588
+  https://www.cluemediator.com/check-a-checkbox-using-react-testing-library
+*/
 describe('Test the FavoritePokemons component', () => {
-  test.only(`if the message No favorite pokemon found is displayed on 
+  test(`if the message No favorite pokemon found is displayed on 
   the screen, if the person does not have favorite pokemons`,
   () => {
     const { container } = render(
@@ -14,4 +23,25 @@ describe('Test the FavoritePokemons component', () => {
 
     expect(paragraph).toHaveTextContent(/^No favorite pokemon found$/i);
   });
+
+  test.only('if all favorite Pokemon cards are displayed',
+    () => {
+      renderWithRouter(<App />);
+      const electricButton = screen.getByRole('button', { name: /^Electric$/i });
+      userEvent.click(electricButton);
+
+      const moreDetailsLink = screen.getByRole('link', { name: /^More details$/i });
+      userEvent.click(moreDetailsLink);
+
+      const favoriteCheckbox = screen.getByRole('checkbox'/* , { name: /^favorite$/i } */);
+
+      expect(favoriteCheckbox).not.toBeChecked();
+      userEvent.click(favoriteCheckbox);
+      expect(favoriteCheckbox).toBeChecked(); // expect(favoriteCheckbox.checked).toEqual(true);
+
+      const favoritesLink = screen.getByRole('link', { name: /^Favorite Pokémons$/i });
+      userEvent.click(favoritesLink);
+      const pokemonName = screen.getByTestId('pokemon-name');
+      expect(pokemonName).toHaveTextContent(/^Pikachu$/i);
+    });
 });
