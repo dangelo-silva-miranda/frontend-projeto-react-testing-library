4273bb213b52a83832402ac9f43e4ec7d02bb8ca
Dângelo-Silva-Miranda <dangelo.silva.miranda@gmail.com>
2021-06-02 16:01:52 -0300

    add: test if the page contains a heading h2 with the text "Page requested not found". Modified file: NotFound.test.js

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,15 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+
+import { NotFound } from '../components';
+
+describe('Test the NotFound component', () => {
+  test(`if the page contains a heading h2 with the text 
+  "Page requested not found"`, () => {
+    render(<NotFound />);
+
+    const heading = screen.getByRole('heading',
+      { level: 2, name: /^Page requested not found/i });
+    expect(heading).toBeInTheDocument();
+  });
+});
