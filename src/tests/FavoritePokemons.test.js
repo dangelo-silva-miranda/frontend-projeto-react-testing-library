3cd286c53323fc1894bc5007b43fba30392729d4
Dângelo-Silva-Miranda <dangelo.silva.miranda@gmail.com>
2021-06-02 15:42:30 -0300

    fix: import pokemons data and refactor tests using only FavoritePokemons component. add: test if no Pokémon card is displayed, if it is not favorited. Modified file: FavoritePokemons.test.js

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,22 +1,22 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
 
 import { FavoritePokemons } from '../components';
-import App from '../App';
 import renderWithRouter from './renderWithRouter';
-
+import pokemons from '../data';
 /*
   Material consultado sobre verificar se checkbox está checked
   https://stackoverflow.com/a/55178588
   https://www.cluemediator.com/check-a-checkbox-using-react-testing-library
 */
 describe('Test the FavoritePokemons component', () => {
+  const [Pikachu, Charmander] = pokemons;
+
   test(`if the message No favorite pokemon found is displayed on 
   the screen, if the person does not have favorite pokemons`,
   () => {
     const { container } = render(
-      <FavoritePokemons />,
+      <FavoritePokemons pokemons={ [] } />,
     );
 
     const paragraph = container.querySelector('p');
@@ -24,24 +24,64 @@ describe('Test the FavoritePokemons component', () => {
     expect(paragraph).toHaveTextContent(/^No favorite pokemon found$/i);
   });
 
-  test.only('if all favorite Pokemon cards are displayed',
+  test('if all favorite Pokemon cards are displayed',
     () => {
-      renderWithRouter(<App />);
+      renderWithRouter(
+        <FavoritePokemons pokemons={ [Pikachu] } />,
+      );
+
+      const pokemonName = screen.getByTestId('pokemon-name');
+      expect(pokemonName).toHaveTextContent(/^Pikachu$/i);
+
+      //= ==========
+      /* renderWithRouter(<App />);
       const electricButton = screen.getByRole('button', { name: /^Electric$/i });
       userEvent.click(electricButton);
 
       const moreDetailsLink = screen.getByRole('link', { name: /^More details$/i });
       userEvent.click(moreDetailsLink);
 
-      const favoriteCheckbox = screen.getByRole('checkbox'/* , { name: /^favorite$/i } */);
+      const favoriteCheckbox = screen.getByRole(
+        'checkbox', { name: /^favorite$/i }
+      );
 
       expect(favoriteCheckbox).not.toBeChecked();
       userEvent.click(favoriteCheckbox);
-      expect(favoriteCheckbox).toBeChecked(); // expect(favoriteCheckbox.checked).toEqual(true);
+      expect(favoriteCheckbox).toBeChecked();
+      // expect(favoriteCheckbox.checked).toEqual(true);
 
       const favoritesLink = screen.getByRole('link', { name: /^Favorite Pokémons$/i });
       userEvent.click(favoritesLink);
       const pokemonName = screen.getByTestId('pokemon-name');
-      expect(pokemonName).toHaveTextContent(/^Pikachu$/i);
+      expect(pokemonName).toHaveTextContent(/^Pikachu$/i); */
+    });
+
+  test('if no Pokémon card is displayed, if it is not favorited',
+    () => {
+      renderWithRouter(
+        <FavoritePokemons pokemons={ [Pikachu] } />,
+      );
+
+      const { name } = Charmander;
+
+      const pokemonName = screen.getByTestId('pokemon-name');
+      expect(pokemonName).not.toHaveTextContent(`${name}`);
+
+      /* const { getByRole, queryByTestId } = renderWithRouter(<App />);
+      const fireButton = getByRole('button', { name: /^Fire$/i });
+      userEvent.click(fireButton);
+
+      const moreDetailsLink = getByRole('link', { name: /^More details$/i });
+      userEvent.click(moreDetailsLink);
+
+      const favoriteCheckbox = getByRole('checkbox');
+
+      expect(favoriteCheckbox).not.toBeChecked();
+
+      const favoritesLink = getByRole('link', { name: /^Favorite Pokémons$/i });
+      userEvent.click(favoritesLink);
+
+      const pokemonName = queryByTestId('pokemon-name');
+      expect(pokemonName).toBeNull(); */
     });
 });
